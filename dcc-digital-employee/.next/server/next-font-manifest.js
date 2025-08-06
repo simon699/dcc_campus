44305c1a8fd5cbd66b11a99d0139732d620fb340
simon699 <shifengwei88@126.com>
2025-08-06 18:31:51 +0800

    更新

--- dcc-digital-employee/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/wedo/Documents/product/DCC数字员工/V1.0/dcc-digital-employee/src/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
